3a60ee98b44144854ad89f654394d4afc50e3666
DAYO KODSON <dkodson23@gmail.com>
2022-02-01 23:48:56 +0100

    update

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,65 +1,75 @@
-import React from 'react'; 
+import React, {useEffect, useState} from 'react'; 
 import  { Link } from "react-router-dom";
+import SubTrans from './SubTrans';
 const TransactionList = (props) => {
+
+     
+
  return (
     <>
-        {
+          {
             props.trans.length > 0 ? 
-
-
+            
             <>
-                <div className="row mb-3">
-                <div className="col">
-                    <h6 className="title">Transactions<br /><small className="fw-normal text-muted">Today, 24 Aug 2021</small>
-                    </h6>
-                </div>
-                <div className="col-auto align-self-center">
-                    <Link to="/transaction" className="small">View all</Link>
-                    </div>
-                </div>
-                <div className="row mb-4">
-                    <div className="col-12">
-                        <div className="card">
-                            <div className="card-body p-0">
-                                <ul className="list-group list-group-flush bg-none">
-                                    {
+                <div className="main-container container">
+                            <div className="row">
+                                <div className="col-12 col-md-12 col-lg-12">
+                                    <div className="card shadow-sm mb-4">
+                                        <div className="card-header">
+                                            <h6 className="my-1">Transactions</h6>
+                                        </div>
+                                        <div className="card-body bg-light">
+                                            <ul className="list-group list-group-flush w-100 bubble-sheet log-information">
 
-                                    
-                                    props.trans ? 
-                                    props.trans.map((function(item) {
-                                                return (
-                                                        <li className="list-group-item">
-                                                            <div className="row">
-                                                                <div className="col-auto">
-                                                                    <div className="avatar avatar-60 shadow-sm card rounded-15 p-1">
-                                                                        <img src="assets/img/company4.jpg" alt="" className="rounded-15" />
-                                                                    </div>
-                                                                </div>
-                                                                <div className="col align-self-center ps-0">
-                                                                    <p className="text-secondary size-10 mb-0">Food</p>
-                                                                    <p>Zomato</p>
-                                                                </div>
-                                                                <div className="col align-self-center text-end">
-                                                                    <p className="text-secondary text-muted size-10 mb-0">15-10-2021 | 10:20am</p>
-                                                                    <p>-25.00</p>
-                                                                </div>
-                                                            </div>
-                                                        </li>
-                                                )
-                                        }))
+                                                {
 
-                                    : <></>
-                                }
-                                    
-                                </ul>
+                                                    
+                                                    props.trans ? 
+                                                    
+                                                    props.trans.map((function(item) {
+                                                           
+                                                              
+                                                               
+                                                            
+                                                               return (
+                                                                        <>
+                                                                        
+                                                                            <SubTrans item={item} id={item.id}/>
+                                                                          
+                                                                        </>
+                                                                        
+                                                                    
+                                                                    )
+                                                               
+                                                               
+                                                            
+                                                        }))
+
+                                                    : <></>
+                                                }
+                
+                                                
+                                                
+                                            </ul>
+                                        </div>
+                                        <div className="card-footer">
+                                            <div className="row">
+                                                <div className="col">
+                                                    
+                                                </div>
+                                                <div className="col-auto">
+                                                    <p className="text-muted small">More</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+
                             </div>
-                        </div>
-                    </div>
                 </div>
-            </>
-
-            : <></>
-        }
+            </> : <></>
+         }
 
      </>
  );
--- src/screens/Splash.js
+++ src/screens/Splash.js
@@ -21,32 +21,33 @@ const Splash = () => {
  return (
     <>
            
-        <section data-page="splash">
-            <main class="container-fluid h-100 bg-3">
+           <section data-page="splash">
+            <main className="container-fluid h-100 bg-3">
             <SplashSvg />
-                <img src="assets/img/logo.png" alt="" class="bg-2-splash" />
+                <img src="assets/img/logo.png" alt="" className="bg-2-splash" />
 
-                <div class="row h-100">
-                    <div class="col-10 col-md-6 col-lg-4 text-left align-self-center mx-auto">
-                        <Link to="/start" class="logo-splash">
-                            <div class="logo-wallet">
-                                <div class="wallet-bottom">
+                <div className="row h-100">
+                    <div className="col-10 col-md-6 col-lg-4 text-left align-self-center mx-auto">
+                        <Link to="/start" className="logo-splash">
+                            <div className="logo-wallet">
+                                <div className="wallet-bottom">
                                 </div>
-                                <div class="wallet-cards"></div>
-                                <div class="wallet-top">
+                                <div className="wallet-cards"></div>
+                                <div className="wallet-top">
                                 </div>
                             </div>
-                            <h1 class="mt-4 mb-2">
-                                <span class="text-secondary fw-light">Millennial</span><br/>
+                            <h1 className="mt-4 mb-2">
+                                <span className="text-secondary fw-light">Millennial</span><br/>
                                 Traders Circle
                             </h1>
-                            <p class="text-secondary">Trade, Invest, Learn</p>
+                            <p className="text-secondary">Trade, Invest, Learn</p>
                         </Link>
                     </div>
                 </div>
             </main>
 
-         </section >   
+ </section >   
+     
      
     
     
